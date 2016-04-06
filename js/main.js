f2896de2ac104894105b3dc0297a880628db68a0
neuquen <kevened@yahoo.com>
2016-04-06 00:01:51 -0400

    Added boilerplate code for all five visualizations. Added html pages for each visualization. Load data for each visualization on each html page.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-// Global data object
-var allData = [];
+// Global data objects
+var robotUnitsData = [], futureEmploymentData = [], stateData = [], marketShareData = [], employmentOccupationData = [];
 
 // Variables for the visualization instances
-var areachart, timeline;
+var robotUnitsSold, futureOfEmployment, byState, marketShare, employmentByOccupation;
 
 // Start application by loading the data
 loadData();
 
 function loadData() {
-    // TODO: We should only need to queue the data if we plan on creating a single-page website. Single or multi-page?
-    d3.queue()
+    // TODO: Restructure so that we don't load every CSV on every page. @Keven
+    var queue = d3_queue.queue();
+
+    queue
         .defer(d3.csv, "data/RobotUnitsSold.csv")
         .defer(d3.csv, "data/The-Future-of-Employment.csv")
         .defer(d3.csv, "data/ByState2014.csv")
@@ -17,11 +19,18 @@ function loadData() {
         .defer(d3.csv, "data/employment-by-occupation.csv")
         .await(prepareData);
 
-    function prepareData(error, robotUnitsSold, futureOfEmployment, byState, marketShareData, employmentByOccupation) {
+    function prepareData(error, robotUnitsSold, futureOfEmployment, byState, marketShare, employmentByOccupation) {
         if (error) throw error;
 
         //Process and clean the data
 
+
+        //Store data in global variable
+        robotUnitsData = robotUnitsSold;
+        futureEmploymentData = futureOfEmployment;
+        stateData = byState;
+        marketShareData = marketShare;
+        employmentOccupationData = employmentByOccupation;
 
         createVis();
     }
@@ -29,5 +38,20 @@ function loadData() {
 
 function createVis() {
 
-    // TO-DO: Instantiate visualization objects here
+    // Instantiate visualization objects here
+    if (typeof RobotUnitsSold == "function") {
+        robotUnitsSold = new RobotUnitsSold("robot-units-sold", robotUnitsData);
+    }
+    if (typeof FutureOfEmployment == "function") {
+        futureOfEmployment = new FutureOfEmployment("future-of-employment", futureEmploymentData);
+    }
+    if (typeof ByState == "function") {
+        byState = new ByState("by-state", stateData);
+    }
+    if (typeof MarketShareData == "function") {
+        marketShare = new MarketShareData("market-share-data", marketShareData);
+    }
+    if (typeof EmploymentByOccupation == "function") {
+        employmentByOccupation = new EmploymentByOccupation("employment-by-occupation", employmentOccupationData);
+    }
 }
