f8b6e40afef30203238f4f87650a326993466b92
neuquen <kevened@yahoo.com>
2016-04-16 15:22:18 -0400

    Added functional bar chart code with minimal styling.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Add the SOC group mapping object
+// @see http://www.bls.gov/soc/major_groups.htm
+var socMap =
+{
+    "11": "Management",
+    "13": "Business and Financial Operations",
+    "15": "Computer and Mathematical",
+    "17": "Architecture and Engineering",
+    "19": "Life, Physical, and Social Science",
+    "21": "Community and Social Services",
+    "23": "Legal",
+    "25": "Education, Training, and Library",
+    "27": "Arts, Design, Entertainment, Sports, and Media",
+    "29": "Healthcare Practitioners and Technical",
+    "31": "Healthcare Support",
+    "33": "Protective Service Occupations",
+    "35": "Food Preparation and Serving Related",
+    "37": "Building and Grounds Cleaning and Maintenance",
+    "39": "Personal Care and Service",
+    "41": "Sales and Related",
+    "43": "Office and Administrative Support",
+    "45": "Farming, Fishing, and Forestry",
+    "47": "Construction and Extraction",
+    "49": "Installation, Maintenance, and Repair",
+    "51": "Production",
+    "53": "Transportation and Material Moving",
+    "55": "Military Specific"
+};
